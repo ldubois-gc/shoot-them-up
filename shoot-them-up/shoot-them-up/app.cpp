fb4484064c460684021b17f2916cbbb2d5644d51
Lea Dubois <dubois.lea06@gmail.com>
2025-09-11 21:28:00 +0200

    fix random

--- shoot-them-up/shoot-them-up/app.cpp
+++ shoot-them-up/shoot-them-up/app.cpp
@@ -63,23 +63,23 @@ void App::Render() {
 
 void App::Start() {
 	gameManager.CreatePlayer(&input, 500.f, 500.f);
-	gameManager.CreateEnemy(50.f, 50.f, 100.f);
+	gameManager.CreateEnemy(50.f, 50.f, 100.f);/*
 	gameManager.CreateEnemy(150.f, 50.f, 100.f);
 	gameManager.CreateEnemy(100.f, 50.f, 100.f);
 	gameManager.CreateObstacle(20.f, 200.f, 200.f, 20.f);
-	gameManager.CreateObstacle(200.f, 20.f, 20.f, 200.f);
+	gameManager.CreateObstacle(200.f, 20.f, 20.f, 200.f);*/
 
-	/*for (int i = 0; i < 20; i++)
+	for (int i = 0; i < 20; i++)
 	{
-		float randomXPos = ((float)rand()) / (float)800;
-		float randomYPos = ((float)rand()) / (float)800;
+		float randomXPos = (float)rand() / (float)RAND_MAX * 800;
+		float randomYPos = (float)rand() / (float)RAND_MAX * 800;
 		gameManager.CreateEnemy(randomXPos, randomYPos, 100.f);
 	}
 
 	for (int i = 0; i < 100; i++)
 	{
-		float randomXPos = ((float)rand()) / (float)800;
-		float randomYPos = ((float)rand()) / (float)800;
+		float randomXPos = (float)rand() / (float)RAND_MAX * 800.0f;
+		float randomYPos = (float)rand() / (float)RAND_MAX * 800;
 		gameManager.CreateObstacle(randomXPos, randomYPos, 20.f, 20.f);
-	}*/
+	}
 }
